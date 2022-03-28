537436cbab7f595b3a97f8e62f8d13f7210a868f
FeliciaFavrholdt <feliciafavrholdt93@gmail.com>
2022-03-28 11:30:02 +0200

    added class DieselTruck.java + constructor matching super

---
+++ src/RentACar/DieselTruck.java
@@ -0,0 +1,7 @@
+package RentACar;
+
+public class DieselTruck extends DieselCar {
+    public DieselTruck(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre, String fuelType, boolean particleFilter) {
+        super(registrationNumber, make, model, numberOfDoors, kmPrLitre, fuelType, particleFilter);
+    }
+}
